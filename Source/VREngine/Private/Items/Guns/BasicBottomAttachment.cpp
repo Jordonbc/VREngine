aa91cb12edf21fca1588d3025419077c49a5fec1
Jordonbc <jordonbc@hotmail.co.uk>
2020-11-26 13:23:11 +0000

    Moved guns to seperate VREngine Guns plugin

--- Source/VREngine/Private/Items/Guns/BasicBottomAttachment.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 2017-2020 Jordon Brooks.
-
-
-#include "Items/Guns/BasicBottomAttachment.h"
-
-ABasicBottomAttachment::ABasicBottomAttachment()
-{
-	Attachment.Position = EAttachmentPosition::Bottom;
-}
--- Source/VREngine/Private/Items/Guns/BasicFrontAttachment.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 2017-2020 Jordon Brooks.
-
-
-#include "Items/Guns/BasicFrontAttachment.h"
-
-ABasicFrontAttachment::ABasicFrontAttachment()
-{
-	Attachment.Position = EAttachmentPosition::Front;
-}
